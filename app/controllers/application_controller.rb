f0d11d42bf3e5a32c979270714d6585dc8c19ab5
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-18 23:06:23 +0330

    Modify the application controller to add authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role])
+  end
 end
