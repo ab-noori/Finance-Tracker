e6bd60fe489844eb2234b8e6fd4a4af1526994da
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-18 23:35:11 +0330

    Add migrations and test files.

---
+++ db/migrate/20230918114819_create_expense_category_associations.rb
@@ -0,0 +1,10 @@
+class CreateExpenseCategoryAssociations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expense_category_associations do |t|
+      t.references :expense, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
