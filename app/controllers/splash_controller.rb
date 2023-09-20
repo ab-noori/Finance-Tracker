720af2640fdb426da83f05fe758a274cdc31769f
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-21 01:35:45 +0330

    Modify the splash view and controller to change the alerts display location.

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,5 +1,7 @@
 class SplashController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
-  def index; end
+  def index
+    redirect_to authenticated_root_path if user_signed_in?
+  end
 end
