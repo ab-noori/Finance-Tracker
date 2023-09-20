6c3b413a762e01568365b00011e7ef2b4c30bd2c
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-21 01:43:44 +0330

    Modify the category model to validate the active storage.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :expense_category_associations
+  has_one_attached :icon, dependent: :destroy
   has_many :expenses, through: :expense_category_associations
 end
