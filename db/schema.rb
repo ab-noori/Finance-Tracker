8a052ab4f9d1edcbdf9d35aad0e48af908d6fe1e
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-22 14:39:14 +0330

    Creating the view for adding new taransactions.

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_122901) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "role", default: "Guest"
+    t.string "role", default: "User"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
