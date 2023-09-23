4150359c50d981e59308d8427240cba4bfbdba64
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-23 19:19:13 +0330

    Add integration test files for controllers.

---
+++ spec/requests/expenses_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe ExpensesController, type: :request do
+  let(:user) { create(:user) }
+  let(:category) { create(:category, user: user) }
+  
+  before do
+    sign_in user
+  end
+
+  describe "GET /new" do
+    it "returns a successful response" do
+      get new_category_expense_path(category)
+      expect(response).to be_successful
+    end
+
+    it "renders the 'new' template" do
+      get new_category_expense_path(category)
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns a new expense" do
+      get new_category_expense_path(category)
+      expect(assigns(:expense)).to be_a_new(Expense)
+    end
+  end
+end
