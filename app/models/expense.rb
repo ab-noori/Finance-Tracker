d3936966e337a948f2c0242bf8e984be0a6ae71e
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-23 19:14:56 +0330

    Add validation to the expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_many :expense_category_associations
   has_many :categories, through: :expense_category_associations
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
