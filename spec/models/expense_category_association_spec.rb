d18283d5f98f106b0019531b346286acbf4f7d14
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-23 19:18:46 +0330

    Add Unit test files for models.

---
+++ spec/models/expense_category_association_spec.rb
@@ -0,0 +1,31 @@
+# spec/models/expense_category_association_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe ExpenseCategoryAssociation, type: :model do
+  describe "associations" do
+    it "belongs to an expense" do
+      association = described_class.reflect_on_association(:expense)
+      expect(association.macro).to eq :belongs_to
+    end
+
+    it "belongs to a category" do
+      association = described_class.reflect_on_association(:category)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
+
+  describe "validations" do
+    it "is not valid without an expense" do
+      category = FactoryBot.create(:category)
+      association = ExpenseCategoryAssociation.new(category: category)
+      expect(association).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      expense = FactoryBot.create(:expense)
+      association = ExpenseCategoryAssociation.new(expense: expense)
+      expect(association).to_not be_valid
+    end
+  end
+end
